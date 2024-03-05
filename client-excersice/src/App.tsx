1651d840577822cdb489aa080daf4e5da8dfcc78
Jennika Elisson <jennika.elisson@gmail.com>
2024-03-05 20:57:35 +0100

    fixing interfaces and cleaning up code

--- client-excersice/src/App.tsx
+++ client-excersice/src/App.tsx
@@ -7,41 +7,44 @@ import axios from "axios";
 import { IImage } from "./models/IImage";
 import { IImagePresentationWithSearchInfo } from "./models/IImagePresent";
 import { SearchForm } from "./components/SearchForm";
-// import { ISearchTime } from "./models/ISearchTime";
-// import { ISpelling } from "./models/ISpelling";
 import { SearchResult } from "./components/SearchResult";
+import { ISearchTime } from "./models/ISearchTime";
+import { ISpelling } from "./models/ISpelling";
+import { Loading } from "./components/Loading";
+import { LoginStart } from "./components/LoginStart";
 
 function App() {
   const [images, setImages] = useState<IImage[] | undefined>([]);
-  const [searchTimer, setSearchTimer] = useState<number | undefined>(undefined);
-  const [spelling, setSpelling] = useState<string | undefined>(undefined);
+  const [searchTimer, setSearchTimer] = useState<ISearchTime | undefined>(
+    undefined
+  );
+  const [spelling, setSpelling] = useState<ISpelling | undefined>(undefined);
   const [loading, setLoading] = useState(false);
 
   const searchImages = async (searchText: string) => {
     setLoading(true);
     try {
-    const response = await axios.get<IImagePresentationWithSearchInfo>(
-      `https://www.googleapis.com/customsearch/v1?key=${
-        import.meta.env.VITE_GOOGLE_API_KEY
-      }&cx=${
-        import.meta.env.VITE_GOOGLE_SEARCH_ENGINE_ID
-      }&num=10&searchType=image&q=${searchText}`
-    );
-    setImages(response.data.items);
-    setSearchTimer(response.data.searchInformation.searchTime);
-    setSpelling(response.data.spelling.correctedQuery)
-  } catch(error) {
-    console.error("Error fetching data:", error);
-    
+      const response = await axios.get<IImagePresentationWithSearchInfo>(
+        `https://www.googleapis.com/customsearch/v1?key=${
+          import.meta.env.VITE_GOOGLE_API_KEY
+        }&cx=${
+          import.meta.env.VITE_GOOGLE_SEARCH_ENGINE_ID
+        }&num=10&searchType=image&q=${searchText}`
+      );
+      setImages(response.data.items);
+      setSearchTimer(response.data.searchInformation);
+      setSpelling(response.data.spelling);
+    } catch (error) {
+      console.error("Error fetching data:", error);
     } finally {
-      setLoading(false); 
+      setLoading(false);
     }
-  }
+  };
 
   const { isAuthenticated, isLoading } = useAuth0();
 
   if (isLoading) {
-    return <div>Loading ...</div>;
+    return <Loading />;
   }
   return (
     <>
@@ -50,19 +53,16 @@ function App() {
           <section>
             <h2>Image Search</h2>
             <SearchForm search={searchImages} />
-            {loading && <p>Loading...</p>}
-            <SearchResult images={images}/>
-            {searchTimer !== undefined && (
-              <div>
-                <p>Search Time: {searchTimer} seconds</p>
-                <p>Suggested spelling: {spelling}</p>
-              </div>
-            )}
+            {loading && <Loading />}
+            <SearchResult images={images} searchTimer={searchTimer} spelling={spelling} />
           </section>
           <LogoutButton />
         </>
       ) : (
-        <LoginButton />
+        <>
+          <LoginStart />
+          <LoginButton />
+        </>
       )}
     </>
   );
--- client-excersice/src/components/SearchResult.tsx
+++ client-excersice/src/components/SearchResult.tsx
@@ -1,16 +1,31 @@
 import { IImage } from "../models/IImage";
+import { ISearchTime } from "../models/ISearchTime";
+import { ISpelling } from "../models/ISpelling";
 
 interface ISearchResultProps {
-    images: IImage[] | undefined;
-  }
-  
-  export const SearchResult = ({ images }: ISearchResultProps) => {
-    return (
+  images: IImage[] | undefined;
+  searchTimer: ISearchTime | undefined;
+  spelling: ISpelling | undefined;
+}
+
+export const SearchResult = ({ images, searchTimer, spelling }: ISearchResultProps) => {
+  return (
+    <div>
+      {images?.map((image, index) => (
+        <div key={index} className="width">
+          <img src={image.link} alt={`Image ${index}`} />
+        </div>
+      ))}
+      {searchTimer !== undefined && (
         <div>
-              {images?.map((image, index) => (
-                <div key={index} className="width">
-                  <img src={image.link} alt={`Image ${index}`} />
-                </div>
-              ))}
-            </div>
-    )}
+          <p>Search Time: {searchTimer.searchTime} seconds</p>
+        </div>
+      )}
+      {spelling !== undefined && (
+        <div>
+          <p>Suggested spelling: {spelling.correctedQuery}</p>
+        </div>
+      )}
+    </div>
+  );
+};
