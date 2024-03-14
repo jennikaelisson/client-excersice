df344e8dc47586d3f63571b2a73d681c837d64cc
Jennika Elisson <jennika.elisson@gmail.com>
2024-03-14 10:54:32 +0100

    added user info

--- client-excersice/src/components/SearchResult.tsx
+++ client-excersice/src/components/SearchResult.tsx
@@ -1,4 +1,5 @@
 // import { useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import { IImage } from "../models/IImage";
 import { ISearchTime } from "../models/ISearchTime";
 import { ISpelling } from "../models/ISpelling";
@@ -10,14 +11,16 @@ interface ISearchResultProps {
 }
 
 export const SearchResult = ({ images, searchTimer, spelling }: ISearchResultProps) => {
-const handleSave = async (image: IImage) => {
+const { user } = useAuth0();
+
+  const handleSave = async (image: IImage) => {
   try {
     const response = await fetch('http://localhost:3002/favorites', {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ imageUrl: image.link }),
+      body: JSON.stringify({ userEmail: user?.email, imageUrl: image.link }),
     });
 
     if (response.ok) {
@@ -36,7 +39,6 @@ const handleSpelling = () => {
 }
   return (
     <div>
-      {" "}
       {spelling !== undefined && (
         <div>
           <p>Did you mean <i onClick={() => handleSpelling()}>{spelling.correctedQuery}</i>?</p>
